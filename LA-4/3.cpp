e47f7c3e88f65203c04f288024e9a49852d4c141
riyacodes1999 <88127997+riyacodes1999@users.noreply.github.com>
2021-10-01 11:02:49 +0530

    Update 3.cpp

--- LA-4/3.cpp
+++ LA-4/3.cpp
@@ -1,16 +1,4 @@
-// #include<iostream>
-// using namespace std;
-// inline int sqr(int x){
-//     return x*x;
-// }
 
-// int main(){
-//     int x;
-//     cout<<"Enter a number : ";
-//     cin>>x;
-//     cout<<"Square of the number : "<<sqr(x);
-//     return 0;
-// }
 
 #include<iostream>
 using namespace std;
@@ -38,3 +26,19 @@ int main(){
     return 0;
 }
  
+
+// OR 
+
+#include<iostream>
+using namespace std;
+inline int sqr(int x){
+    return x*x;
+}
+
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    cout<<"Square of the number : "<<sqr(x);
+    return 0;
+}
